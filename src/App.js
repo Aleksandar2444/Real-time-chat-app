0ef9e440e193359f79e65b4bffb76aac69817c80
Aleksandar <aleksandarichev@github.com>
2022-11-05 10:54:35 +0100

    zoom disabled in meta viewport

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import "./App.css";
 import MainPage from "./MainPage/MainPage";
 import Navbar from "./Navbar/Navbar";
@@ -5,6 +6,12 @@ import Navbar from "./Navbar/Navbar";
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Helmet>
       <Navbar />
       <MainPage />
     </div>
