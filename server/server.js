79764aa33b7a319a790f2f6a5c50115b38528093
Aleksandar <aleksandarichev@github.com>
2022-11-05 09:57:44 +0100

    server added

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const { createServer } = require("http");
+const server = createServer(app);
+
+const { Server } = require("socket.io");
+const port = 3001;
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("join_room", ({ user, room }) => {
+    socket.join(room);
+  });
+
+  socket.on("send_msg", ({ room, user, message }) => {
+    const dialog = { user: user, message: message };
+    socket.to(room).emit("receive_msg", dialog);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port: ${port}`);
+});
